965e67c426c59c4ba40f6950cf00c404a267e6b3
rashi <rashid058@mail.ru>
2021-08-07 00:12:04 +0500

    Test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,7 +10,8 @@ public class Main {
         System.out.print("Введите число Арабское/Римское от 1 до 10" + "\n");
         Scanner reader = new Scanner(System.in);
         System.out.print("Пример ввод 2 + 2/I + II: ");
-        if (reader.hasNextInt() == true) {
+
+        if (reader.hasNextInt()) {
             ArabicNumeric arabicNumeric = new ArabicNumeric(reader);
         } else {
             RomanNumeric romanNumeric = new RomanNumeric(reader);
